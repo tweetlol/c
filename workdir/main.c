fc64a0e5be6a4a52c2c0d7ce9d5a2e03958aead7
FJ <rzrtwt@gmail.com>
2023-06-25 16:46:13 +0200

    osrs v2.3

--- workdir/main.c
+++ workdir/main.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<X11/Xlib.h>
+#include<X11/keysym.h>
 #include<X11/extensions/XTest.h>
 
 int Dt(int delta){
@@ -9,17 +10,13 @@ int Dt(int delta){
 	return(delta);
 }
 void Click(Display *screen){
-	int delta=0;
-
 	XTestFakeButtonEvent(screen,Button1,True,0);
 	XFlush(screen);
-	usleep(10000 * delta);
-	delta = Dt(delta);
+	usleep(30000);
 	XTestFakeButtonEvent(screen,Button1,False,0);
 	XFlush(screen);
 }
-void FindUI(Display* screen,Window targetWindow){	//najde levý horní roh UI, pro automatizaci UI manipulace vztaženo o oknu (multibox?)
-//	Window window; //najít target window pomocí Xlib funkcí...
+void FindUI(Display* screen,Window targetWindow){
 	XWindowAttributes targetWindowAttr;
 	XGetWindowAttributes(screen,targetWindow,&targetWindowAttr);
 
@@ -27,15 +24,18 @@ void FindUI(Display* screen,Window targetWindow){	//najde levý horní roh UI, p
 
 }
 
-void DropBag(Display* screen,Window targetWindow){
+void DropBag(Display* screen,Window targetWindow,KeyCode shift){
 	int delta=0;
 	int i=0;
 	int j=0;
+
+	XTestFakeKeyEvent(screen,shift,True,CurrentTime);
+	XFlush(screen);
 	while(i<4){
 		while(j<6){
-			XWarpPointer(screen,None,targetWindow,0,0,0,0,587+i*43,277+j*29);
+			XWarpPointer(screen,None,targetWindow,0,0,0,0,590+i*43,301+j*36);
 			j++;
-			usleep(100000 * delta);
+			usleep(170000 * (1+delta));
 			delta = Dt(delta);
 			printf("dropping slot %ix%i\n",i,j);
 			XFlush(screen);
@@ -44,38 +44,64 @@ void DropBag(Display* screen,Window targetWindow){
 		j=0;
 		i++;
 	}
-}// NEXT UP : XWarpPointer for cursor placement
+	XTestFakeKeyEvent(screen,shift,False,CurrentTime);
+}
 
+void Mine(Display *screen,Window targetWindow){
+	int i=0;
+	int j=0;
+	int delta=0;
 
+	while(i<10){
+		XWarpPointer(screen,None,targetWindow,0,0,0,0,390,220);
+		XFlush(screen);
+		Click(screen);
+		sleep(4);
+		usleep(12345 * delta);
+		XWarpPointer(screen,None,targetWindow,0,0,0,0,500,350);
+		XFlush(screen);
+		Click(screen);
+		sleep(4);
+		usleep(13254 * delta);
+		delta = Dt(delta);
+		i++;
+	}
+}
 
+//	THE INT MAIN
 int main(){
-	printf("Click the target Runelite window...\n");
+	
 	Display *screen = XOpenDisplay(NULL);
 	if(screen == NULL){
 		printf("Unable to open display.\n");
 		return(1);
 	}
+
 	Window rootWindow = DefaultRootWindow(screen);
 	Window targetWindow;
 	XEvent event;
-	printf("load screen var, rootWindow var, proceeding to grabbing a pointer and waiting for event"\n);
-//	find the RL window by CLICKING IT
+	KeyCode shift = XKeysymToKeycode(screen,XK_Shift_L);
+
+	//	find the RL window by CLICKING IT
+	printf("Click the target Runelite window...\n");
 	XGrabPointer(screen,rootWindow,False,ButtonPressMask,GrabModeAsync,GrabModeAsync,None,None,CurrentTime);
 	XNextEvent(screen,&event);
 
 	targetWindow = event.xbutton.subwindow;
 	if(targetWindow == None){
 		targetWindow = event.xbutton.window;
-		printf("targetWindow set to %lu\n",targetWindow);
 	}
 	XUngrabPointer(screen,CurrentTime);
-	printf("ungrabbing pointer\n");
+	printf("targetWindow set to %lu\n",targetWindow);
+
 //	prints window attributes and id for checks, 
 	FindUI(screen,targetWindow);
-	printf("sleeping for a second, then bagdropping\n");
 	sleep(1);
-
-	DropBag(screen,targetWindow);
+	
+	while(1){
+		Mine(screen,targetWindow);
+		DropBag(screen,targetWindow,shift);
+	}
 
 	XCloseDisplay(screen);
 	return(0);
