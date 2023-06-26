032e5d88f9d4ce677f8b0d2f2c0d5768fe4ffabd
FJ <rzrtwt@gmail.com>
2023-06-26 16:32:17 +0200

    osrsproj 2.3

--- workdir/main.c
+++ workdir/main.c
@@ -9,21 +9,25 @@ int Dt(int delta){
 	delta = (delta + 1) % 3;
 	return(delta);
 }
+//clicks the screen (and unclicks)
 void Click(Display *screen){
+	usleep(5000);
 	XTestFakeButtonEvent(screen,Button1,True,0);
 	XFlush(screen);
-	usleep(30000);
+	usleep(10000);
 	XTestFakeButtonEvent(screen,Button1,False,0);
 	XFlush(screen);
+	usleep(5000);
 }
+//window-size check
 void FindUI(Display* screen,Window targetWindow){
 	XWindowAttributes targetWindowAttr;
 	XGetWindowAttributes(screen,targetWindow,&targetWindowAttr);
-
-	printf("window.attr.id=%lu\nwindow.attr.width=%i\nwindow.attr.height=%i\n",targetWindow,targetWindowAttr.width,targetWindowAttr.height);
+	printf("  window.attr.id=%lu\n  window.attr.width=%i\n  window.attr.height=%i\n",targetWindow,targetWindowAttr.width,targetWindowAttr.height);
+	printf("expected: 821x558\n");
 
 }
-
+//empties line 2 and lower of inventory (keeps line 1)
 void DropBag(Display* screen,Window targetWindow,KeyCode shift){
 	int delta=0;
 	int i=0;
@@ -34,10 +38,10 @@ void DropBag(Display* screen,Window targetWindow,KeyCode shift){
 	while(i<4){
 		while(j<6){
 			XWarpPointer(screen,None,targetWindow,0,0,0,0,590+i*43,301+j*36);
-			j++;
-			usleep(170000 * (1+delta));
+			usleep(120000 * (1+delta));
 			delta = Dt(delta);
 			printf("dropping slot %ix%i\n",i,j);
+			j++;
 			XFlush(screen);
 			Click(screen);
 		}
@@ -46,7 +50,7 @@ void DropBag(Display* screen,Window targetWindow,KeyCode shift){
 	}
 	XTestFakeKeyEvent(screen,shift,False,CurrentTime);
 }
-
+//mining loop (2 targets N+W)
 void Mine(Display *screen,Window targetWindow){
 	int i=0;
 	int j=0;
@@ -67,10 +71,24 @@ void Mine(Display *screen,Window targetWindow){
 		i++;
 	}
 }
-
-//	THE INT MAIN
-int main(){
+//woodcutting loop (1 target N)
+void Woodcut(Display *screen,Window targetWindow){
+	int i=0;
+	int delta=0;
 	
+	while(i<20){
+		XWarpPointer(screen,None,targetWindow,0,0,0,0,390,220);
+		XFlush(screen);
+		Click(screen);
+		sleep(10+delta);
+		delta = Dt(delta);
+		i++;
+	}
+}
+
+//THE INT MAIN
+int main(){
+	int run;
 	Display *screen = XOpenDisplay(NULL);
 	if(screen == NULL){
 		printf("Unable to open display.\n");
@@ -82,7 +100,7 @@ int main(){
 	XEvent event;
 	KeyCode shift = XKeysymToKeycode(screen,XK_Shift_L);
 
-	//	find the RL window by CLICKING IT
+//	find the RL window by CLICKING IT
 	printf("Click the target Runelite window...\n");
 	XGrabPointer(screen,rootWindow,False,ButtonPressMask,GrabModeAsync,GrabModeAsync,None,None,CurrentTime);
 	XNextEvent(screen,&event);
@@ -92,17 +110,24 @@ int main(){
 		targetWindow = event.xbutton.window;
 	}
 	XUngrabPointer(screen,CurrentTime);
-	printf("targetWindow set to %lu\n",targetWindow);
 
 //	prints window attributes and id for checks, 
 	FindUI(screen,targetWindow);
 	sleep(1);
-	
-	while(1){
-		Mine(screen,targetWindow);
-		DropBag(screen,targetWindow,shift);
+	printf("1: mining (4sec, 20cycles, North+West)\n2: woodcutting(10+sec, 20cycles, North)\n);
+	scanf("%i",&run);
+	switch(run){
+	case 1:
+		while(1){
+			Mine(screen,targetWindow);
+			DropBag(screen,targetWindow,shift);
+	 	}
+	case 2:
+		while(1){
+			Mine(screen,targetWindow);
+			DropBag(screen,targetWindow,shift);
+		}
 	}
-
 	XCloseDisplay(screen);
 	return(0);
 }
